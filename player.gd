54b51f173067cc8ec5b5fd5c42f183a3a59a1578
CrashingFootfalls <152283718+CrashingFootfalls@users.noreply.github.com>
2025-01-23 10:16:15 -0600

    movement
    movement

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed := 200
+
+func _physics_process(delta):
+	var direction := Vector2.ZERO
+	
+	if Input.is_action_pressed("Left"):
+		direction.x += 1
+		
+	if Input.is_action_pressed("Right"):
+		direction.x -= 1
+		
+	if Input.is_action_pressed("Up"):
+		direction.y += 1
+		
+	if Input.is_action_pressed("Down"):
+		direction.y -= 1
+		
